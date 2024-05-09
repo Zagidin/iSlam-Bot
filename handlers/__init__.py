f28f32ad3d9e0f10dde4529bcbca2510f48df423
Zagidin <prap0235@gmail.com>
2024-05-10 01:56:44 +0300

    Zagidin

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,10 +1,5 @@
 from .admin import dp
-from .start import dp
-from .help import dp
-from .surah import dp
-from .nasheed import dp
-from .search import dp
-from .search_browser import dp
-from .feedback import dp
+from .user import dp
+
 
 __all__ = ["dp"]
---
+++ handlers/user/__init__.py
@@ -0,0 +1,9 @@
+from .start import dp
+from .help import dp
+from .surah import dp
+from .nasheed import dp
+from .search import dp
+from .search_browser import dp
+from .feedback import dp
+
+__all__ = ["dp"]
